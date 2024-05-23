7282e037fddfda44bf5b645aaaeb7c7ba0cf3b88
ylahti6 <ylahti6@gmail.com>
2024-05-23 09:08:37 +0100

    textContent added to component

---
+++ src/assets/textContext.js
@@ -0,0 +1,11 @@
+const textContent = {
+
+    // --- NAV CONTENT
+    name: "yannick.",
+    works: "Works",
+    about: "About",
+    contact: "Contact",
+    timeFeature: "London (GMT)"
+}
+
+export default textContent;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,17 @@
+import textContent from '../assets/textContext';
+
 import './Nav.scss';
 
 function Nav() {
+const {name, works, about, contact, timeFeature} = textContent;
     return (
         <nav>
             <ul>
-                <li>testing</li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>{name}</li>
+                <li>{works}</li>
+                <li>{about}</li>
+                <li>{contact}</li>
+                <li>{timeFeature}</li>
             </ul>
         </nav>
     );
