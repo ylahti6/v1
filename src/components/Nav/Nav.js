72ae95e18cc5345548cc5e09409fbbee8ec0aa18
ylahti6 <ylahti6@gmail.com>
2024-05-24 10:49:34 +0100

    nav is responsive

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,33 +1,45 @@
 import { useState } from 'react';
-import textContent from '../../assets/textContext'; // Fixed typo: 'textContext' to 'textContent'
-// import NavTime from './features/featureTime';
+import textContent from '../../assets/textContext';
 import './Nav.scss';
 
 function Nav() {
-    const { name, works, about, contact } = textContent;
+  const { name, works, about, contact } = textContent;
+  const [isChecked, setIsChecked] = useState(false);
+    
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+  };
 
-    return (
-        <nav className="nav">
-            <input type="checkbox" id="nav-check" />
-            <div className="nav-header">
-                <div className="nav-title">{name}</div>
-            </div>
+  return (
+    <nav className={`nav ${isChecked ? 'checked' : ''}`}>
+      <input 
+      type="checkbox" 
+      id="nav-check" 
+      checked={isChecked}
+      onChange={handleToggle}
+      />
+      
+      <div className="nav-header">
+        <div className="nav-title">{name}</div>
+      </div>
 
-            <div className="nav-btn">
-                <label htmlFor="nav-check">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </label>
-            </div>
+      <div className="nav-btn">
+        <label htmlFor="nav-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+      
+      <ul className="nav-list">
+        <li><a href="#works">{works}</a></li>
+        <li><a href="#about">{about}</a></li>
+        <li><a href="#contact">{contact}</a></li>
+      </ul>
 
-            <ul className="nav-list">
-                <li><a href="#works">{works}</a></li>
-                <li><a href="#about">{about}</a></li>
-                <li><a href="#contact">{contact}</a></li>
-            </ul>
-        </nav>
-    );
+      <div className="overlay"></div>
+    </nav>
+  );
 }
 
 export default Nav;
