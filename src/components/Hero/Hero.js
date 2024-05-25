0d8427e86512aa44cdca07fdb9455b7fd7648437
ylahti6 <ylahti6@gmail.com>
2024-05-25 13:07:52 +0100

    hero needs a word change feature

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,10 +2,11 @@ import textContext from '../../assets/textContext';
 import './Hero.scss';
 
 function Hero() {
-const {header } = textContext;
+const {header, mongoDB } = textContext;
     return(
         <div className='hero'>
-        <h1>{header}</h1>
+            <h1>{header}</h1>
+            <h1>{mongoDB}</h1>
         </div>
     );
 }
