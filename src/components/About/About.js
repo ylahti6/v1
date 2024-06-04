45013d002b3028ee6d1bdf6142fa9c566a25415e
ylahti6 <ylahti6@gmail.com>
2024-06-04 20:38:12 +0100

    event listener added to about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,31 @@
+import { useEffect } from 'react';
 import textContext from '../../assets/textContent';
 import './About.scss';
+import { useState } from 'react';
 
 function About() {
 const {aboutTag, aboutContent} = textContext;
-const spaces = Array(28).fill('\u00a0').join('');
+const [spaces, setSpaces] = useState(getSpaces());
+
+function getSpaces() {
+    let spaces;
+    if (window.innerWidth <= 640){
+        spaces = Array(14).fill('\u00a0').join('');
+    }
+    else {
+        spaces = Array(28).fill('\u00a0').join('');
+    }
+    return spaces;
+}
+
+useEffect(() => {
+    function handleResize() {
+        setSpaces(getSpaces());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);  
+}, []);
     return(
         <div className='about'>
         <p>{aboutTag}</p>
